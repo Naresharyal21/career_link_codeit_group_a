dcb287e8322916c2471e015805d2c98ebb3f3b78
ChandanWebDevelops <chandanshrestha2077@gmail.com>
2026-08-11 13:30:49 +0545

    limiting the token period to 5hours in settings.py

--- Backend/career_link_project/career_link_project/settings.py
+++ Backend/career_link_project/career_link_project/settings.py
@@ -144,3 +144,11 @@ REST_FRAMEWORK = {
         "rest_framework.permissions.IsAuthenticated",
     ],
 }
+
+from datetime import timedelta
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(hours=5),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ROTATE_REFRESH_TOKENS": False,
+}
